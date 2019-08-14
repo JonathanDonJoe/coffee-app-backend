69137ca22f403192c7bfcd3b3ad4dedf4de7cf29
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-08-14 13:45:53 -0400

    Removed extraneous coffee class

--- models/coffee.js
+++ models/coffee.js
@@ -1,40 +1,19 @@
 const db = require('./conn');
 
-class Coffee {
-    constructor(id, name, bespoke, size) {
-        this.id = id;
-        this.name = name;
-        this.bespoke = bespoke;
-        this.size = size;
-    }
-    static getAll() {
-        return db.any(`SELECT * FROM coffee`)
-            // .then((oneCoffeeData) => {
-            //     return oneCoffeeData;
-            // })
-            .catch( (e) => {
-                console.log(e);
-                return null;
-            })
-    }
-    static getById(id) {
-        return db.one(`SELECT * FROM coffee WHERE id =${id}`)
-            // .then((oneCoffeeData) => {
-            //     return oneCoffeeData;
-            // })
-            .catch( (e) => {
-                console.log(e);
-                return null;
-            })
-    }
-    save() {
-        return db.result(`
-            UPDATE coffee SET
-            name=${this.name},
-            bespoke=${this.bespoke},
-            size=${this.size},
-            where id=${this.id}
-        `);
-    }
-};
-module.exports = Coffee;
+
+function getAll() {
+    return db.any(`SELECT * FROM coffee`)
+        .catch( (e) => {
+            console.log(e);
+            return null;
+        })
+}
+function getById(id) {
+    return db.one(`SELECT * FROM coffee WHERE id =${id}`)
+        .catch( (e) => {
+            console.log(e);
+            return null;
+        })
+}
+
+module.exports = {getAll, getById};
